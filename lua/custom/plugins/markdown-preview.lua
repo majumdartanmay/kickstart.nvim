3f0bdd6b831002986fad4e1d2c3facd1865bf5c9
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-02-04 23:29:10 +0530

    Set the tab width
    Date:      Sat Feb 3 23:17:44 2024 +0530
    
    Author:    Tanmay Majumdar <tanmaymajumdar5612@gmail.com>
    Date:      Sat Feb 3 23:17:44 2024 +0530

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,8 +1,8 @@
-return {
+return  {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end
+    build = function() vim.fn["mkdp#util#install"]() end,
   }
 }
