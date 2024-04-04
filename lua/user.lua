172116da9e6a41d7d42819cb821c41724e8b75c7
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-04-04 09:27:14 +0530

    Change visual mode highlighting colors

--- lua/user.lua
+++ lua/user.lua
@@ -145,3 +145,7 @@ vim.keymap.set('n', '<leader>3l', ':Git log<CR>', { noremap = true, silent = tru
 
 -- Relative number
 vim.wo.relativenumber = true
+
+-- Configure visual mode highlighting
+vim.cmd([[:hi Visual  guifg=White guibg=LightBlue gui=none]])
+
