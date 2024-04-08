e7106d8f387f07f6ed792ec80288ed4e0dbf5d54
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-04-08 15:36:40 +0530

    Override java configs

--- lua/custom/plugins/config/java.lua
+++ lua/custom/plugins/config/java.lua
@@ -1,11 +1,11 @@
 local function create_config() 
   local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t');
-  local workspace_dir = './bin/data' .. project_name
+  local workspace_dir = 'C:/Users/tanmay.majumdar/AppData/Local/jdtls/data/' .. project_name
 
   local config = {
     cmd = {
 
-      '/home/tanmay/.sdkman/candidates/java/21.0.2-tem/bin/java', 
+      'C:/Program Files/Java/jdk-17/bin/java', 
       '-Declipse.application=org.eclipse.jdt.ls.core.id1',
       '-Dosgi.bundles.defaultStartLevel=4',
       '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -15,8 +15,8 @@ local function create_config()
       '--add-modules=ALL-SYSTEM',
       '--add-opens', 'java.base/java.util=ALL-UNNAMED',
       '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-      '-jar', '/home/tanmay/Personal/jdtls_utility/bin/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-      '-configuration', './bin/config_linux',
+      '-jar', 'C:/Users/tanmay.majumdar/AppData/Local/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+      '-configuration', 'C:/Users/tanmay.majumdar/AppData/Local/jdtls/config_win',
       '-data', workspace_dir
     },
   }
@@ -32,5 +32,4 @@ local function start_jdtls()
   print("Attempt to start JDLTS finished " .. project_name);
 end
 
-vim.keymap.set('n', '<C-1>', start_jdtls, { noremap = true, silent = true, desc = "Start JDTLS server" })
-
+vim.keymap.set('n', '<M-j>', start_jdtls, { noremap = true, silent = true, desc = "Start JDTLS server" })
